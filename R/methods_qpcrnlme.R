6371aabec614b5531ec531bfc7e46ddfe404b5c8
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2013-06-21 14:19:59 +0200

    coef and vcov methods

--- R/methods_qpcrnlme.R
+++ R/methods_qpcrnlme.R
@@ -7,3 +7,16 @@ print.ddct <- function(x, digits = 3, ...){
   cat("\ndelta delta c(t) estimates:\n")
   printCoefmat(x$coefmat, digits=digits, P.values=TRUE, has.Pvalue=TRUE)
 }
+
+coef.nlmect <- function(object, ...){
+  return(object$ct[,1])
+}
+coefficients.nlmect <- function(object, ...){
+  return(object$ct[,1])
+}
+
+vcov.nlmect <- function(object, ...){
+  vc <- object$vcov
+  rownames(vc) <- colnames(vc) <- names(object$ct[,1])
+  return(vc)
+}
